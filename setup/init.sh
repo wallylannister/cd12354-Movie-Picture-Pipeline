ff38f2cb6b54580ce0f635f93c2929ae00c7f2e4
wallylannister <tuongvo2808@gmail.com>
2024-07-29 02:31:18 +0700

    test

--- setup/init.sh
+++ setup/init.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e -o pipefail
+set -x
 
 echo "Fetching IAM github-action-user ARN"
 userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
 
 # Download tool for manipulating aws-auth
 echo "Downloading tool..."
-curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
+curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_darwin_amd64 -o aws-iam-authenticator
 chmod +x aws-iam-authenticator
 
 echo "Updating permissions"
