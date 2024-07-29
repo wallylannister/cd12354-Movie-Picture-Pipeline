ce85acec43f789b17c45ee687d91fd878ebb27f1
wallylannister <tuongvo2808@gmail.com>
2024-07-29 23:41:31 +0700

    v

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -1,33 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import PropTypes from 'prop-types';
-// import axios from 'axios';
-
-// function MovieList({ onMovieClick }) {
-//   const [movies, setMovies] = useState([]);
-
-//   useEffect(() => {
-//     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-//       setMovies(response.data.movies);
-//     });
-//   }, []);
-
-//   return (
-//     <ul>
-//       {movies.map((movie) => (
-//         <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
-//           {movie.title}
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// MovieList.propTypes = {
-//   onMovieClick: PropTypes.func.isRequired,
-// };
-
-// export default MovieList;
-
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
@@ -36,20 +6,14 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
-      .then((response) => {
-        console.log(response.data); // Check the structure here
-        setMovies(response.data.movies);
-      })
-      .catch((error) => {
-        console.error('Error fetching movies:', error);
-        setMovies([]); // Set to empty array on error
-      });
+    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
+      setMovies(response.data.movies);
+    });
   }, []);
 
   return (
     <ul>
-      {Array.isArray(movies) && movies.map((movie) => (
+      {movies.map((movie) => (
         <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
           {movie.title}
         </li>
@@ -63,3 +27,39 @@ MovieList.propTypes = {
 };
 
 export default MovieList;
+
+// import React, { useState, useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import axios from 'axios';
+
+// function MovieList({ onMovieClick }) {
+//   const [movies, setMovies] = useState([]);
+
+//   useEffect(() => {
+//     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
+//       .then((response) => {
+//         console.log(response.data); // Check the structure here
+//         setMovies(response.data.movies);
+//       })
+//       .catch((error) => {
+//         console.error('Error fetching movies:', error);
+//         setMovies([]); // Set to empty array on error
+//       });
+//   }, []);
+
+//   return (
+//     <ul>
+//       {Array.isArray(movies) && movies.map((movie) => (
+//         <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+//           {movie.title}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+// MovieList.propTypes = {
+//   onMovieClick: PropTypes.func.isRequired,
+// };
+
+// export default MovieList;
